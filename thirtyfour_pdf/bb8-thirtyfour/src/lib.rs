91f1178a87288972737301eb4ec31a706c186621
Alexandru Nechita <alexandru.italia32@gmail.com>
2024-07-23 22:46:43 +0200

    feat(print): Add pdf generation

---
+++ thirtyfour_pdf/bb8-thirtyfour/src/lib.rs
@@ -0,0 +1,250 @@
+#![warn(rust_2018_idioms)]
+
+//! [`bolt_client`] support for the [`bb8`] connection pool.
+//!
+//! # Example
+//! ```
+//! use std::env;
+//!
+//! # use bb8::ManageConnection;
+//! use bb8_bolt::{
+//!     bb8::Pool,
+//!     bolt_client::Metadata,
+//!     bolt_proto::{version::*, Value},
+//!     Manager,
+//! #   bolt_client::error::{ConnectionError, Error as ClientError},
+//! #   bolt_proto::message::Success,
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     // Configure a connection manager. We'll request Bolt v4.4 or v4.3.
+//!     let manager = Manager::new(
+//!         env::var("BOLT_TEST_ADDR")?,
+//!         env::var("BOLT_TEST_DOMAIN").ok(),
+//!         [V4_4, V4_3, 0, 0],
+//!         Metadata::from_iter(vec![
+//!             ("user_agent", "bolt-client/X.Y.Z"),
+//!             ("scheme", "basic"),
+//!             ("principal", &env::var("BOLT_TEST_USERNAME")?),
+//!             ("credentials", &env::var("BOLT_TEST_PASSWORD")?),
+//!         ]),
+//!     ).await?;
+//!
+//! #   match manager.connect().await {
+//! #       Err(ClientError::ConnectionError(ConnectionError::HandshakeFailed(versions))) => {
+//! #           println!("skipping test: {}", ConnectionError::HandshakeFailed(versions));
+//! #           return Ok(());
+//! #       }
+//! #       Err(other) => panic!("{}", other),
+//! #       _ => {}
+//! #   }
+//!     // Create a connection pool. This should be shared across your application.
+//!     let pool = Pool::builder().build(manager).await?;
+//!
+//!     // Fetch and use a connection from the pool
+//!     let mut conn = pool.get().await?;
+//!     let response = conn.run("RETURN 1 as num;", None, None).await?;
+//! #   Success::try_from(response.clone()).unwrap();
+//!     let pull_meta = Metadata::from_iter(vec![("n", 1)]);
+//!     let (records, response) = conn.pull(Some(pull_meta)).await?;
+//! #   Success::try_from(response).unwrap();
+//!     assert_eq!(records[0].fields(), &[Value::from(1)]);
+//!
+//!     Ok(())
+//! }
+
+use std::{io, net::SocketAddr};
+
+use async_trait::async_trait;
+use bb8::ManageConnection;
+use tokio::{
+    io::BufStream,
+    net::{lookup_host, ToSocketAddrs},
+};
+use tokio_util::compat::*;
+
+use bolt_client::{
+    error::{CommunicationError, ConnectionError, Error as ClientError},
+    Client, Metadata, Stream,
+};
+use bolt_proto::{error::Error as ProtocolError, message, Message, ServerState};
+
+pub use bb8;
+pub use bolt_client;
+pub use bolt_client::bolt_proto;
+
+#[derive(Debug)]
+pub struct Manager {
+    addr: SocketAddr,
+    domain: Option<String>,
+    version_specifiers: [u32; 4],
+    metadata: Metadata,
+}
+
+impl Manager {
+    pub async fn new(
+        addr: impl ToSocketAddrs,
+        domain: Option<String>,
+        version_specifiers: [u32; 4],
+        metadata: Metadata,
+    ) -> io::Result<Self> {
+        Ok(Self {
+            addr: lookup_host(addr)
+                .await?
+                .next()
+                .ok_or_else(|| io::Error::from(io::ErrorKind::AddrNotAvailable))?,
+            domain,
+            version_specifiers,
+            metadata,
+        })
+    }
+}
+
+#[async_trait]
+impl ManageConnection for Manager {
+    type Connection = Client<Compat<BufStream<Stream>>>;
+    type Error = ClientError;
+
+    async fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        let mut client = Client::new(
+            BufStream::new(
+                Stream::connect(self.addr, self.domain.as_ref())
+                    .await
+                    .map_err(ConnectionError::from)?,
+            )
+            .compat(),
+            &self.version_specifiers,
+        )
+        .await?;
+
+        // TODO: Should we send HELLO now, or let the user do it later?
+        match client.hello(self.metadata.clone()).await? {
+            Message::Success(_) => Ok(client),
+            other => Err(CommunicationError::from(io::Error::new(
+                io::ErrorKind::ConnectionAborted,
+                format!("server responded with {:?}", other),
+            ))
+            .into()),
+        }
+    }
+
+    async fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
+        message::Success::try_from(conn.reset().await?).map_err(ProtocolError::from)?;
+        Ok(())
+    }
+
+    fn has_broken(&self, conn: &mut Self::Connection) -> bool {
+        conn.server_state() == ServerState::Defunct
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use bb8::*;
+    use bolt_proto::{version::*, Value};
+    use futures_util::{stream::FuturesUnordered, StreamExt};
+
+    use super::*;
+
+    async fn get_connection_manager(version_specifiers: [u32; 4], succeed: bool) -> Manager {
+        let credentials = if succeed {
+            env::var("BOLT_TEST_PASSWORD").unwrap()
+        } else {
+            String::from("invalid")
+        };
+
+        Manager::new(
+            env::var("BOLT_TEST_ADDR").unwrap(),
+            env::var("BOLT_TEST_DOMAIN").ok(),
+            version_specifiers,
+            Metadata::from_iter(vec![
+                ("user_agent", "bolt-client/X.Y.Z"),
+                ("scheme", "basic"),
+                ("principal", &env::var("BOLT_TEST_USERNAME").unwrap()),
+                ("credentials", &credentials),
+            ]),
+        )
+        .await
+        .unwrap()
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn basic_pool() {
+        const POOL_SIZE: usize = 15;
+        const MAX_CONNS: usize = 50;
+
+        for &bolt_version in &[V1_0, V2_0, V3_0, V4_0, V4_1, V4_2, V4_3, V4_4, V4] {
+            let manager = get_connection_manager([bolt_version, 0, 0, 0], true).await;
+
+            // Don't even test connection pool if server doesn't support this Bolt version
+            match manager.connect().await {
+                Err(ClientError::ConnectionError(ConnectionError::HandshakeFailed(versions))) => {
+                    println!(
+                        "skipping test: {}",
+                        ConnectionError::HandshakeFailed(versions)
+                    );
+                    continue;
+                }
+                Err(other) => panic!("{}", other),
+                _ => {}
+            }
+
+            let pool = Pool::builder()
+                .max_size(POOL_SIZE as u32)
+                .build(manager)
+                .await
+                .unwrap();
+
+            (0..MAX_CONNS)
+                .map(|i| {
+                    let pool = pool.clone();
+                    async move {
+                        let mut client = pool.get().await.unwrap();
+                        let statement = format!("RETURN {} as num;", i);
+                        client.run(statement, None, None).await.unwrap();
+                        let (records, response) = client
+                            .pull(Some(Metadata::from_iter(vec![("n", 1)])))
+                            .await
+                            .unwrap();
+                        assert!(message::Success::try_from(response).is_ok());
+                        assert_eq!(records[0].fields(), &[Value::from(i as i8)]);
+                    }
+                })
+                .collect::<FuturesUnordered<_>>()
+                .collect::<Vec<_>>()
+                .await;
+        }
+    }
+
+    #[tokio::test]
+    async fn invalid_init_fails() {
+        for &bolt_version in &[V1_0, V2_0, V3_0, V4_0, V4_1, V4_2, V4_3, V4_4, V4] {
+            let manager = get_connection_manager([bolt_version, 0, 0, 0], false).await;
+            match manager.connect().await {
+                Ok(_) => panic!("initialization should have failed"),
+                Err(ClientError::ConnectionError(ConnectionError::HandshakeFailed(versions))) => {
+                    println!(
+                        "skipping test: {}",
+                        ConnectionError::HandshakeFailed(versions)
+                    );
+                    continue;
+                }
+                Err(ClientError::CommunicationError(comm_err)) => {
+                    if let CommunicationError::IoError(io_err) = &*comm_err {
+                        if io_err.kind() == io::ErrorKind::ConnectionAborted {
+                            // Test passed. We only check the first compatible version since
+                            // sending too many invalid credentials will cause us to get
+                            // rate-limited.
+                            return;
+                        }
+                    }
+                    panic!("{}", comm_err);
+                }
+                Err(other) => panic!("{}", other),
+            }
+        }
+    }
+}
--- thirtyfour_pdf/src/main.rs
+++ thirtyfour_pdf/src/main.rs
@@ -1,34 +1,23 @@
 use axum::{
-    body::Bytes, extract::{FromRef, State}, http::{Result, StatusCode}, response::{Response, IntoResponse}, routing::{get, post}, Json, Router
+    extract::{State},
+    response::{IntoResponse, Response},
+    routing::{get}, Router,
 };
 // use serde::{Deserialize, Serialize};
-use serde_json::map;
 
 use std::sync::Arc;
-use tokio::{signal, sync::{RwLock, OnceCell}};
-use tokio::time::{sleep, Duration};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use webdriver::command::{WebDriverCommand, PrintParameters};
 use thirtyfour::prelude::*;
+use tokio::{
+    sync::{RwLock},
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use webdriver::command::PrintParameters;
 //
-// #[derive(Clone, Debug)]
-// struct AppState {
-//     webdriver_client: Arc<RwLock<Client>>,
-// }
-//
-// static BLANK_TAB: OnceCell<WindowHandle> = OnceCell::const_new();
-//
-// async fn init_blank_tab(client: &Arc<RwLock<Client>>,) -> &WindowHandle {
-//     BLANK_TAB.get_or_init(|| async {
-//         let window: WindowHandle = {
-//             // lock client
-//             let wd = client.read().await;
-//             // get blank page
-//             wd.windows().await.unwrap().get(0).unwrap().clone()
-//         };
-//         window
-//     }).await
-// }
+#[derive(Clone, Debug)]
+struct AppState {
+    webdriver_client: Arc<RwLock<WebDriver>>,
+    webdriver_client2: Arc<RwLock<WebDriver>>,
+}
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
@@ -36,119 +25,109 @@ const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 async fn main() {
     // initialize tracing
     tracing_subscriber::registry()
-    .with(
-        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-            format!("{}=debug,tower_http=debug,axum=trace", PKG_NAME).into()
-        }),
-    )
-    .with(tracing_subscriber::fmt::layer())
-    .init();
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug,axum=trace", PKG_NAME).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:4444", caps).await.expect("Cannot open connection with webdriver, check if webdriver proxy is running and check if address and port are matching");
-    
-    driver.
-    //
-    //
-    // // Navigate to https://wikipedia.org.
-    // driver.goto("https://wikipedia.org").await?;
-    // let elem_form = driver.find(By::Id("search-form")).await?;
-    //
-    // // Find element from element.
-    // let elem_text = elem_form.find(By::Id("searchInput")).await?;
-    //
-    // // Type in the search terms.
-    // elem_text.send_keys("selenium").await?;
-    //
-    // // Click the search button.
-    // let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
-    // elem_button.click().await?;
-    //
-    // // Look for header to implicitly wait for the page to load.
-    // driver.find(By::ClassName("firstHeading")).await?;
-    // assert_eq!(driver.title().await?, "Selenium - Wikipedia");
-    //
-    // // Always explicitly close the browser.
-    sleep(Duration::from_millis(10000)).await;
-    driver.quit().await;
-    //
-    // Ok(())
-    //
-    //
-    // let webdriver_client: Client = ClientBuilder::rustls()
-    // .expect("Failed to initialize rustls")
-    // .capabilities(make_capabilities(Browers::Firefox))
-    // .connect("http://localhost:4444")
-    // .await.expect("couldn't connect to the client builder");
-    //
-    // let state = AppState {
-    //     webdriver_client: Arc::new(RwLock::new(webdriver_client))
-    // };
-    //
-    // init_blank_tab(&state.webdriver_client).await;
-    //
-    // // build our application with a route
-    // let app = Router::new()
-    //     .route("/", get(generate_pdf))
-    //     .route("/2", get(generate_pdf2))
-    //     .with_state(state);
-    //
-    // // run our app with hyper, listening globally on port 3000
-    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    // axum::serve(listener, app).await.unwrap();
-    // 
-    // ()
+
+    let driver = WebDriver::new("http://localhost:4444", caps.clone()).await.expect("Cannot open connection with webdriver, check if webdriver proxy is running and check if address and port are matching");
+
+    let driver2 = WebDriver::new("http://localhost:4444", caps.clone()).await.expect("Cannot open connection with webdriver, check if webdriver proxy is running and check if address and port are matching");
+
+    let state = AppState {
+        webdriver_client: Arc::new(RwLock::new(driver.clone())),
+        webdriver_client2: Arc::new(RwLock::new(driver2.clone())),
+    };
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(generate_pdf))
+        .route("/2", get(generate_pdf2))
+        .with_state(state);
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    // Always explicitly close the browser.
+    //sleep(Duration::from_millis(10000)).await;
+    //driver.quit().await;
 }
 
-// async fn generate_pdf(State(state): State<AppState>) -> Response {
-//     let window = open_new_tab(&state.webdriver_client, "https://kozea.github.io/WeasyPerf/samples/columns/columns.html").await;
-//     print_window(&state.webdriver_client, window).await.into_response()
-// }
-//
-// async fn generate_pdf2(State(state): State<AppState>) -> Response {
-//     let window = open_new_tab(&state.webdriver_client, "https://kozea.github.io/WeasyPerf/samples/odyssey/odyssey.html").await;
-//     print_window(&state.webdriver_client, window).await.into_response()
-// }
-//
-// async fn open_new_tab(client: &Arc<RwLock<Client>>, url: &str) -> NewWindowResponse {
-//     let tab = {
-//         // lock webdriver
-//         let wd = client.write().await;
-//
-//         // switch to window
-//         let tab = wd.new_window(true).await.expect("switch");
-//
-//         // open url in new tab
-//         wd.goto(url).await.expect("close failed");
-//         tab
-//     };
-//
-//     // return tab
-//     tab
-// }
-//
-// async fn print_window(client: &Arc<RwLock<Client>>, window: NewWindowResponse) -> Bytes {
-//     let cmd = WebDriverCommand::Print(PrintParameters::default());
-//     let pdf = {
-//         // lock webdriver
-//         let wd = client.write().await;
-//
-//         // switch to window
-//         wd.switch_to_window(window.handle).await.expect("switch");
-//
-//         // print window
-//         let buff: String = wd.issue_cmd(cmd).await.expect("failed print pdf").to_string();
-//
-//         // close window
-//         wd.close_window().await.expect("close failed");
-//
-//         // switch to blank window
-//         wd.switch_to_window(BLANK_TAB.get().expect("blank_tab_not_initialized").clone()).await.expect("switch");
-//         buff
-//     };
-//     
-//     Bytes::from(pdf)
-// }
+const ODYSSEY: &str = include_str!("base64_other.txt");
+
+async fn generate_pdf(State(state): State<AppState>) -> Response {
+    let tmp = ODYSSEY.trim();
+    //tracing::info!("{tmp:?}");
+
+    tracing::info!("tab create");
+    let window = {
+        let driver = state.webdriver_client.read().await;
+        driver.new_tab().await
+    }
+    .unwrap();
+    tracing::info!("tab ended {window:?}");
+
+    tracing::info!("window switch");
+    {
+        let driver = state.webdriver_client.write().await;
+        driver.switch_to_window(window.clone()).await.unwrap();
+        driver.goto(tmp).await.unwrap();
+    }
+    tracing::info!("window switch ended");
+
+    tracing::info!("pdf started");
+
+    let pdf = {
+        let driver = state.webdriver_client.write().await;
+        driver.switch_to_window(window).await.unwrap();
+        driver.print_page_base64(PrintParameters::default()).await
+    };
+
+    let pdf = pdf.unwrap();
+    tracing::info!("pdf ended");
+
+    pdf.into_response()
+}
+
+async fn generate_pdf2(State(state): State<AppState>) -> Response {
+    let tmp = ODYSSEY.trim();
+    //tracing::info!("{tmp:?}");
+
+    tracing::info!("tab create");
+    let window = {
+        let driver = state.webdriver_client2.read().await;
+        driver.new_tab().await
+    }
+    .unwrap();
+    tracing::info!("tab ended {window:?}");
+
+    tracing::info!("window switch");
+    {
+        let driver = state.webdriver_client2.write().await;
+        driver.switch_to_window(window.clone()).await.unwrap();
+        driver.goto(tmp).await.unwrap();
+    }
+    tracing::info!("window switch ended");
+
+    tracing::info!("pdf started");
+
+    let pdf = {
+        let driver = state.webdriver_client2.write().await;
+        driver.switch_to_window(window).await.unwrap();
+        driver.print_page_base64(PrintParameters::default()).await
+    };
+
+    let pdf = pdf.unwrap();
+    tracing::info!("pdf ended");
+
+    pdf.into_response()
+}
 
 // async fn shutdown_signal() {
 //     let ctrl_c = async {
